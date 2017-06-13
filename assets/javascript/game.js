e5c26becff788521a106ef78e9b1bcf10fe05a62
Dylan Ishihara <dylan.ishihara@gmail.com>
2017-06-13 15:40:03 -0700

    beginning bootstrap layout

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+var wordbase = [];
+var pickedLetters = [];
+var wrongLetters =[]
+
+var wins = 0;
+var losses = 0;
+
+var answer = options[Math.floor(Math.random() * wordbase.length)];
+
+for (var i = 0; i < answer.length; i++){
+
+}
+
+document.onkeyup = function(event){
+	var userGuess = event.key
+}
