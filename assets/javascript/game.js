9a44c78ff1a39eec9c09992321e4e563f9049a4c
Dylan Ishihara <dylan.ishihara@gmail.com>
2017-06-14 09:57:01 -0700

    pushing empty space to array and joining to print on HTML

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,19 @@
-var wordbase = [];
+
+var wordbase = ["apple", "orange", "class", "facebook"];
 var pickedLetters = [];
 var wrongLetters =[]
 
 var wins = 0;
 var losses = 0;
-
-var answer = options[Math.floor(Math.random() * wordbase.length)];
-
-
-document.getElementById("word").innerHTML()
-
+// randomly generate answer
+var answer = wordbase[Math.floor(Math.random() * wordbase.length)];
+var chosenWord = []
+// setting up underscores
+for (var i = 0; i < answer.length; i++) {
+	chosenWord.push("_")
+};
+// 
+document.getElementById("word").innerHTML = chosenWord.join(" ");
 
 document.onkeyup = function(event){
 	var userGuess = event.key
